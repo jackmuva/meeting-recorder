d3e6335dd89955313f568c453478e077c554861e
jackmuva <jackmu@umich.edu>
2026-06-09 12:09:39 -0700

    Initial Commit

---
+++ meeting-recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meeting-recorder
+//
+//  Created by Jack Mu on 6/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meeting-recorder/meeting_recorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  meeting_recorderApp.swift
+//  meeting-recorder
+//
+//  Created by Jack Mu on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct meeting_recorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
