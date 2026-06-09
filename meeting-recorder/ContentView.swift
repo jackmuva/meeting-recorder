3b1da5da26e6c78aba25dfabfa700bb3695288a1
jackmuva <jackmu@umich.edu>
2026-06-09 13:37:48 -0700

    have the plaud sdk in the projct

--- meeting-recorder/ContentView.swift
+++ meeting-recorder/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Jack!").padding().background(Color.yellow, in: RoundedRectangle(cornerRadius: 5))
         }
         .padding()
     }
